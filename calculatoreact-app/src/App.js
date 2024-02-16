e87f8f1dbe1b8bb8023729c9076cb519e4fd05fe
samarthbluelupin <145488981+samcoder07@users.noreply.github.com>
2024-02-16 12:15:58 +0530

    checkin

---
+++ calculatoreact-app/src/App.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import "./App.css";
+import Button from "./components/Button";
+import Input from "./components/Input";
+
+import * as math from "mathjs";
+const App = () => {
+  const [text, setText] = useState("");
+  const [result, setResult] = useState("");
+
+  const addToText = (val) => {
+    setText((text) => [...text, val + " "]);
+  };
+
+  const calculateResult = () => {
+    const input = text.join("");
+
+    setResult(math.evaluate(input));
+  };
+
+  const resetHandler = () => {
+    setText("");
+    setResult("");
+  };
+
+  const buttonColor = "#f2a33c";
+  return (
+    <div className="App">
+      <div className="calc-wrapper">
+        <Input result={result} text={text} />
+        <div className="row">
+          <Button symbol="7" handleClick={addToText} />
+          <Button symbol="8" handleClick={addToText} />
+          <Button symbol="9" handleClick={addToText} />
+          <Button symbol="/" color={buttonColor} />
+        </div>
+        <div className="row">
+          <Button symbol="4" handleClick={addToText} />
+          <Button symbol="5" handleClick={addToText} />
+          <Button symbol="6" handleClick={addToText} />
+          <Button symbol="*" color={buttonColor} />
+        </div>
+        <div className="row">
+          <Button symbol="1" handleClick={addToText} />
+          <Button symbol="2" handleClick={addToText} />
+          <Button symbol="3" handleClick={addToText} />
+          <Button symbol="+" color={buttonColor} />
+        </div>
+        <div className="row">
+          <Button symbol="0" />
+          <Button symbol="." />
+          <Button symbol="=" handleClick={calculateResult} />
+          <Button symbol="-" color={buttonColor} handleClick={addToText} />
+        </div>
+        <Button symbol="Clear" color="red" handleClick={resetHandler}></Button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
